ab43581de9d400bf594b41105d86d8afcf70afa6
nhusn <139863367+nhusn@users.noreply.github.com>
2023-12-18 22:09:05 +0530

    frontend

--- src/app/all-products/all-products.component.ts
+++ src/app/all-products/all-products.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-all-products',
   templateUrl: './all-products.component.html',
   styleUrls: ['./all-products.component.css']
 })
-export class AllProductsComponent {
+export class AllProductsComponent implements OnInit{
+  allProducts:any=[]
+  constructor(private api:ApiService){}
+
+  ngOnInit(): void {
+    this.api.getAllProductAPI().subscribe((res:any)=>{
+      this.allProducts = res
+    })
+  }
 
 }
