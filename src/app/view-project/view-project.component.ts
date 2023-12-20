fe01a871845c237c6db840a05a02e18b98fa4313
nhusn <139863367+nhusn@users.noreply.github.com>
2023-12-20 11:29:10 +0530

    view product

--- src/app/view-project/view-project.component.ts
+++ src/app/view-project/view-project.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToasterService } from '../services/toaster.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-project',
   templateUrl: './view-project.component.html',
   styleUrls: ['./view-project.component.css']
 })
-export class ViewProjectComponent {
+export class ViewProjectComponent implements OnInit{
 
+  constructor(private toaster:ToasterService,private route:ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((res:any)=>{
+      const {id}=res
+      console.log(id);
+      // get details of particular product
+    })
+  }
+
+  addtoWishlist(products:any){
+    if(sessionStorage.getItem("token")){
+      this.toaster.showSuccess('proceed to login');
+    }else{
+      this.toaster.showWarning('Please login');
+      
+    }
+  }
+
+  addtoCart(products:any){
+    if(sessionStorage.getItem("token")){
+      this.toaster.showSuccess('proceed to login');
+    }else{
+      this.toaster.showWarning('Please login');
+      
+    }
+  }
 }
