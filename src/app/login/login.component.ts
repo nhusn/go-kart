26299d0160794045ab704dd6da8698ffe350002b
nhusn <139863367+nhusn@users.noreply.github.com>
2023-12-20 10:35:14 +0530

    login logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import { ToasterService } from '../services/toaster.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +11,35 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm = this.fb.group({
+    email:['',[Validators.required,Validators.email]],
+    password:['',[Validators.required,Validators.pattern('[a-zA-Z0-9 ]*')]],
+  })
+  constructor(private fb:FormBuilder,private api:ApiService,private toaster:ToasterService,private router:Router){}
+
+  login(){    
+    if(this.loginForm.valid){
+      const email = this.loginForm.value.email
+      const password = this.loginForm.value.password
+      const user = {email,password}
+      
+      this.api.loginAPI(user).subscribe({
+        next:(res:any)=>{
+          console.log(res);
+          
+          this.toaster.showSuccess(`${res.existingUser.username} Login succesfully`)
+          sessionStorage.setItem('username',res.existingUser.username)
+          sessionStorage.setItem('token',res.token)
+          this.loginForm.reset()
+          this.router.navigateByUrl('') 
+        },
+        error:(err:any)=>{
+          this.toaster.showWarning(err.error)          
+        }
+      }) 
+    }else{
+      this.toaster.showWarning('Invalid Input')
+    }
+       
+  }
 }
